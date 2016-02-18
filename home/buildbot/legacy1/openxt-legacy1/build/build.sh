62ed26cd79fe5eaeff36e38f6f684829aad436c7
oxtbuilder <oxtbuilder@github.com>
2016-02-18 17:27:45 -0500

    Attempt to link to the filer for cache, windows tools and build output
    Signed-off-by: oxtbuilder <oxtbuilder@github.com>

--- home/buildbot/legacy1/openxt-legacy1/build/build.sh
+++ home/buildbot/legacy1/openxt-legacy1/build/build.sh
@@ -9,17 +9,17 @@ git clone file:///home/buildbot/legacy1/openxt-legacy1/build/git/openxt.git
 cd openxt
 git checkout $BRANCH
 cp -r ../../certs .
-#mkdir wintools
-#rsync -r buildbot@192.168.0.10:/home/build/win/$BRANCH/ wintools/
-#WINTOOLS="`pwd`/wintools"
-#WINTOOLS_ID="`grep -o '[0-9]*' wintools/BUILD_ID`"
+mkdir wintools
+rsync -r builds@158.69.227.117:/home/builds/win/$BRANCH/ wintools/
+WINTOOLS="`pwd`/wintools"
+WINTOOLS_ID="`grep -o '[0-9]*' wintools/BUILD_ID`"
 mv /tmp/git_heads_$BUILDID git_heads
 cp example-config .config
 cat <<EOF >> .config
 BRANCH=$BRANCH
 NAME_SITE="oxt"
-OPENXT_MIRROR="http://mirror.openxt.org"
-OE_TARBALL_MIRROR="http://mirror.openxt.org"
+OPENXT_MIRROR="http://158.69.227.117/mirror"
+OE_TARBALL_MIRROR="http://158.69.227.117/mirror"
 OPENXT_GIT_MIRROR="/home/buildbot/legacy1/openxt-legacy1/build/git"
 OPENXT_GIT_PROTOCOL="file"
 REPO_PROD_CACERT="/home/buildbot/legacy1/openxt-legacy1/build/certs/prod-cacert.pem"
@@ -28,9 +28,9 @@ REPO_DEV_SIGNING_CERT="/home/buildbot/legacy1/openxt-legacy1/build/certs/dev-cac
 REPO_DEV_SIGNING_KEY="/home/buildbot/legacy1/openxt-legacy1/build/certs/dev-cakey.pem"
 WIN_BUILD_OUTPUT="$WINTOOLS"
 XC_TOOLS_BUILD=$WINTOOLS_ID
-SYNC_CACHE_OE=192.168.0.10:/home/build/oe
-BUILD_RSYNC_DESTINATION=192.168.0.10:/home/build/builds
-NETBOOT_HTTP_URL=http://192.99.200.146/builds
+SYNC_CACHE_OE=158.69.227.117:/home/builds/oe
+BUILD_RSYNC_DESTINATION=158.69.227.117:/home/builds/builds
+NETBOOT_HTTP_URL=http://158.69.227.117/builds
 EOF
 #./do_build.sh -i $BUILDID -s setupoe,sync_cache
 ./do_build.sh -i $BUILDID | tee build.log
