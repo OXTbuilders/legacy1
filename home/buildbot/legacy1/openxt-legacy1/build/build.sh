7915e9e083e2392f66f801b221984103120e9065
oxtbuilder <oxtbuilder@github.com>
2016-02-26 12:36:47 -0500

    sync_cache_back after a successful build and fix last_build
    Signed-off-by: oxtbuilder <oxtbuilder@github.com>

--- home/buildbot/legacy1/openxt-legacy1/build/build.sh
+++ home/buildbot/legacy1/openxt-legacy1/build/build.sh
@@ -28,7 +28,7 @@ REPO_DEV_SIGNING_CERT="/home/buildbot/legacy1/openxt-legacy1/build/certs/dev-cac
 REPO_DEV_SIGNING_KEY="/home/buildbot/legacy1/openxt-legacy1/build/certs/dev-cakey.pem"
 WIN_BUILD_OUTPUT="$WINTOOLS"
 XC_TOOLS_BUILD=$WINTOOLS_ID
-SYNC_CACHE_OE=builds@158.69.227.117:/home/builds/oe
+SYNC_CACHE_OE=builds@158.69.227.117:/home/builds/oe/legacy
 BUILD_RSYNC_DESTINATION=builds@158.69.227.117:/home/builds/builds
 NETBOOT_HTTP_URL=http://158.69.227.117/builds
 EOF
